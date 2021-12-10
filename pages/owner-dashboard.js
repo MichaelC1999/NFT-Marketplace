a60d2a6179279bfbd2972becb011aaace5e8d54c
Michael <michaelcarroll1999@gmail.com>
2021-12-10 16:52:30 -0500

    log

--- pages/owner-dashboard.js
+++ pages/owner-dashboard.js
@@ -30,7 +30,7 @@ export default function CreatorDashboard() {
   function verifyAccount(selectedAddress) {
     console.log(selectedAddress?.toUpperCase() !== marketOwnerAddr?.toUpperCase(), selectedAddress)
     if (selectedAddress?.toUpperCase() !== marketOwnerAddr?.toUpperCase()) {
-      Router.push('/');
+      // Router.push('/');
     }
   }
 
